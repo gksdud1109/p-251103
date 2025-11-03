6fc5fa8d14680f334632d6ad1fef4ad5d266ea6a
gksdud1109 <vectorh532@gmail.com>
2025-11-03 10:50:04 +0900

    refactor: to kotlin - homeController

--- backend/src/main/java/com/back/domain/home/home/controller/HomeController.kt
+++ backend/src/main/java/com/back/domain/home/home/controller/HomeController.kt
@@ -1,47 +1,45 @@
-package com.back.domain.home.home.controller;
+package com.back.domain.home.home.controller
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Collections;
-import java.util.Map;
-import java.util.stream.Collectors;
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.servlet.http.HttpSession
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.net.InetAddress
+import java.net.UnknownHostException
+import java.util.*
+import java.util.function.Function
+import java.util.stream.Collectors
 
 @RestController
-@Tag(name="Home", description = "홈 컨트롤러")
-public class HomeController {
-
-    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+@Tag(name = "Home", description = "홈 컨트롤러")
+class HomeController {
+    @GetMapping(produces = [MediaType.TEXT_HTML_VALUE])
     @Operation(summary = "홈 화면", description = "홈 화면입니다.")
-    public String home() throws UnknownHostException {
-
-        InetAddress localhost = InetAddress.getLocalHost();
+    fun home(): String {
+        val localhost = InetAddress.getLocalHost()
 
         return """
                 <h1>Welcome to Rest1</h1>
-                <p>Server IP Address: %s</p>
-                <p>Server Host Name: %s</p>
+                <p>Server IP Address: ${localhost.hostAddress}</p>
+                <p>Server Host Name: ${localhost.hostName}</p>
                 <div>
                     <a href="swagger-ui/index.html">API 문서로 이동</a>
                 </div>
-                """.formatted(localhost.getHostAddress(), localhost.getHostName());
+                
+                """.trimIndent()
     }
 
     @GetMapping("/session")
     @Operation(summary = "세션 확인용")
-    public Map<String, Object> session(HttpSession session) {
+    fun session(session: HttpSession): Map<String, Any> {
 
-        return Collections.list(session.getAttributeNames()).stream()
-                .collect(Collectors.toMap(
-                        name -> name,
-                        session::getAttribute
-                ));
+        return session
+            .attributeNames
+            .toList()
+            .associateWith {
+                session.getAttribute(it)!!
+            }
     }
-    
 }
